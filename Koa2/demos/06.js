a90312f737abf1f65660f04d0e3cb29bdfeed235
chenjinrui <871967481@qq.com>
2018-03-18 18:33:19 +0800

    koa-route 模块

---
+++ Koa2/demos/06.js
@@ -0,0 +1,17 @@
+const route = require('koa-route');
+const Koa = require('koa');
+const app = new Koa();
+
+const about = ctx => {
+  ctx.response.type = 'html';
+  ctx.response.body = '<a href="/">Index Page</a>';
+};
+
+const main = ctx => {
+  ctx.response.body = 'Hello Koa2';
+};
+
+app.use(route.get('/', main));
+app.use(route.get('/about'), about);
+
+app.lisen(3300);
